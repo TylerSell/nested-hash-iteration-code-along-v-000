0fe7233990c8179709fc63e7000ef1434d3afe78
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-08-27 22:19:54 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,9 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, details|
     details.each do |key, value|
-      binding.pry
+      if key == :favorite_ice_cream_flavors
+        value.delete_if {|ice_cream| ice_cream == "strawberry"}
+      end
     end
   end
 end
